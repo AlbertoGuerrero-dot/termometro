915e5914397b5e44bba3ae2d0622c179851c15c8
Luis Alberto <delacruz.guerrero.luisalberto@gmail.com>
2023-05-06 21:48:59 -0600

    server and client added

--- server/index.js
+++ server/index.js
@@ -1,10 +1,26 @@
+const express = require('express');
+const socketIo = require('socket.io');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', function(socket){
+    console.log("Nuevo socket conectado");
+})
+
+app.get('/', (req, res, next) =>{
+    res.sendFile(__dirname + '/index.html');
+});
+
 const SerialPort = require('serialport').SerialPort;
 const { DelimiterParser } = require('@serialport/parser-delimiter');
 
 const puerto = new SerialPort({
     path: 'COM4',
     baudRate: 9600,
-})
+});
 
 const parser = puerto.pipe(new DelimiterParser({ delimiter: '\n'}))
 
@@ -15,10 +31,21 @@ parser.on('open', function(){
 parser.on('data', function(data){
     var enc = new TextDecoder();
     var arr = new Uint8Array(data);
-    var ready = enc.decode(arr)
+    var ready = enc.decode(arr);
     console.log(ready);
+    io.emit('arduino:data', {
+        value: ready
+    });
+    //var temperatura = ready.match(/\d+/);
+    // Actualiza la etiqueta h6 con la temperatura
+    //var temperaturaElem = document.getElementById('temperatura');
+    //temperaturaElem.textContent = "Temperatura: " + temperatura + " grados Celsius";
 });
 
 puerto.on('error', function(err){
     console.log(err);
-})
+});
+
+server.listen(3000, () => {
+    console.log("Servidor en puerto 3000");
+});
--- termo/termo.ino
+++ termo/termo.ino
@@ -12,9 +12,9 @@ void loop() {
   float resistance = seriesResistor / ((1023 / (float)reading) - 1);  // Calcula la resistencia del termistor
   float temperature = 1 / ((log(resistance / seriesResistor) / B) + (1 / 298.15)) - 273.15;  // Calcula la temperatura en grados Celsius
   temp = (int)temperature;    // Convierte la temperatura a un entero
-  Serial.print("Temperatura: ");     // Imprime el mensaje de temperatura en la conexión serial
+  //Serial.print("Temperatura: ");     // Imprime el mensaje de temperatura en la conexión serial
   Serial.print(temp);
-  Serial.println(" grados Celsius");
+  //Serial.println(" grados Celsius");
   Serial.print('\n');
-  delay(2000);                // Espera un segundo antes de leer la temperatura nuevamente
+  delay(2500);                // Espera un segundo antes de leer la temperatura nuevamente
 }
